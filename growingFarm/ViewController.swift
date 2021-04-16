44f01466eaa8f6d7caca0808ad771b084e113900
michael <qazwsx1926305@gmail.com>
2021-04-16 20:44:24 +0800

    Initial Commit

---
+++ growingFarm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  growingFarm
+//
+//  Created by Michael on 2021/4/16.
+//  Copyright © 2021 Michael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
